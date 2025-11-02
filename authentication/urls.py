7ec91a44ef7d3a2305138ed4a0db4ce3a471e785
itsjasminn <muvvahhida@gmail.com>
2025-11-02 19:32:51 +0500

    new

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,11 +8,11 @@ from authentication.views import LogoutView, ChangePasswordFormView, ProfileUpda
 urlpatterns = [
     path('login', LoginFormView.as_view(), name='login'),
     path('login/verify-code', VerifyCodeFormView.as_view(), name='verify_code'),
-    path('login/login_w_passwd', LoginWithPasswordFormView.as_view(), name="login_w_passwd"),
+    path('login/login_w_passwd', LoginWithPasswordFormView.as_view(), name="login_with_password"),
     path('account/logout', LogoutView.as_view(), name='logout'),
     path('account/profile/<int:pk>', ProfileUpdateView.as_view(), name='profile'),
     path('account/profile/profile-photo/<int:pk>', UpdateProfilePhotoView.as_view(), name='profile_image'),
-    path('account/profile/change-passwd/<int:pk>', ChangePasswordFormView.as_view(), name='change_passwd'),
+    path('account/profile/change-passwd/<int:pk>', ChangePasswordFormView.as_view(), name='change_password'),
     path('account/profile/change_email', ChangeMailFormView.as_view(), name='change_email'),
     path('account/profile/change_email/change_email_verify_code', ChangeMailCodeFormView.as_view(), name='get_code'),
     path('account/propfile/contact-w-tg', TemplateView.as_view(template_name="profile/contact_w_tg.html"),
